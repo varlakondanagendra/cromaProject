d18d68b6791d5e276d78d60815cd27b620aa550e
vbnagendra <varlakondanagendra@gmail.com>
2024-02-15 13:31:26 +0530

    4th commit

---
+++ src/main/java/generic/navigateToApplication.java
@@ -0,0 +1,22 @@
+package generic;
+
+import java.time.Duration;
+
+import org.apache.commons.logging.impl.SLF4JLogFactory;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+import io.github.bonigarcia.wdm.WebDriverManager;
+
+public class navigateToApplication {
+
+	public static void main(String[] args) {
+		WebDriverManager.chromedriver().setup();
+		WebDriver driver=new ChromeDriver();
+		driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(3));
+		driver.manage().window().maximize();
+		driver.get("https://fireflink.com");
+		driver.quit();
+	}
+
+}
